f98da6944e92d9114c2d2f56086727875e4589a8
Alexei Lozovsky <me@ilammy.net>
2020-12-01 15:34:24 +0900

    Reference implementation of Soter algorithm ID
    Throw it in for completeness. This one is easy to implement and the spec
    is very clear so I won't reference it directly. However, other systems
    working with Soter algorithm IDs will benefit from this part for sure.

---
+++ themis/spec/common/soter-alg.go
@@ -0,0 +1,74 @@
+package common
+
+// AlgorithmID encodes symmetric algorithm parameters for Soter.
+type AlgorithmID uint32
+
+const (
+	algorithmMask   = 0xF0000000
+	algorithmOffset = 28
+	kdfMask         = 0x0F000000
+	kdfOffset       = 24
+	paddingMask     = 0x000F0000
+	paddingOffset   = 16
+	keyLengthMask   = 0x00000FFF
+	keyLengthOffset = 0
+)
+
+// SymmetricAlgorithm indicates symmetric encryption algorithm in AlgorithmID.
+type SymmetricAlgorithm int
+
+// Supported SymmetricAlgorithm values.
+const (
+	AesECB SymmetricAlgorithm = iota + 1
+	AesCBC
+	AesXTS
+	AesGCM
+)
+
+// KeyDerivationFunction indicates key derivation function in AlgorithmID.
+type KeyDerivationFunction int
+
+// Supported KeyDerivationFunction values.
+const (
+	NoKDF KeyDerivationFunction = iota
+	PBKDF2HmacSha256
+)
+
+// PaddingAlgorithm indicates padding algorithm in AlgorithmID.
+type PaddingAlgorithm int
+
+// Supported PaddingAlgorithm values.
+const (
+	NoPadding PaddingAlgorithm = iota
+	PKCS7Padding
+)
+
+// MakeAlgorithmID constructs algorithm ID from components.
+func MakeAlgorithmID(algorithm SymmetricAlgorithm, kdf KeyDerivationFunction, padding PaddingAlgorithm, keyBits int) AlgorithmID {
+	var value uint32
+	value |= (uint32(algorithm) & (algorithmMask >> algorithmOffset)) << algorithmOffset
+	value |= (uint32(kdf) & (kdfMask >> kdfOffset)) << kdfOffset
+	value |= (uint32(padding) & (paddingMask >> paddingOffset)) << paddingOffset
+	value |= (uint32(keyBits) & (keyLengthMask >> keyLengthOffset)) << keyLengthOffset
+	return AlgorithmID(value)
+}
+
+// Algorithm returns symmetric algorithm component.
+func (id AlgorithmID) Algorithm() SymmetricAlgorithm {
+	return SymmetricAlgorithm((id & algorithmMask) >> algorithmOffset)
+}
+
+// KDF returns key derivation function.
+func (id AlgorithmID) KDF() KeyDerivationFunction {
+	return KeyDerivationFunction((id & kdfMask) >> kdfOffset)
+}
+
+// Padding returns padding algorithm.
+func (id AlgorithmID) Padding() PaddingAlgorithm {
+	return PaddingAlgorithm((id & paddingMask) >> paddingOffset)
+}
+
+// KeyBits returns size of the key in bits.
+func (id AlgorithmID) KeyBits() int {
+	return int((id & keyLengthMask) >> keyLengthOffset)
+}
